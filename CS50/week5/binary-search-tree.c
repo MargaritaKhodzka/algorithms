3d3baa68ff845ac1b030286a3cd4e8e036ca8ea5
MargaritaKhodzka <>
2022-10-01 14:51:39 -0400

    binary tree and binary search tree

---
+++ CS50/week5/binary-search-tree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool search(node *tree, int number)
+{
+    if (tree == NULL)
+    {
+        return false;
+    }
+    else if (number < tree->number)
+    {
+        return search(tree->left, number);
+    }
+    else if (number > tree->number)
+    {
+        return search(tree->right, number);
+    }
+    else
+    {
+        return true;
+    }
+}
